bb9a69b9441ae059b1292f80025435856517e682
Egor Opekin <vip.opekin@mail.ru>
2025-09-20 16:39:49 +0600

    feat(auth): add logout endpoint

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Request, Response, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.dependencies import get_current_user
 from src.database import get_db_session
 from src.auth.service import UserService
-from src.auth.schemas import UserAuth
+from src.users.schemas import UserCreate
 from src.auth.utils import set_token_cookies
+from src.users.models import User
 
 
 router = APIRouter(
@@ -16,7 +18,7 @@ router = APIRouter(
 @router.post('/register')
 async def register_user(
     response: Response,
-    user_data: UserAuth,
+    user_data: UserCreate,
     session: AsyncSession = Depends(get_db_session),
 ):
     access_token = await UserService.register_user(
@@ -31,7 +33,7 @@ async def register_user(
 @router.post('/login')
 async def login_user(
     response: Response,
-    user_data: UserAuth,
+    user_data: UserCreate,
     session: AsyncSession = Depends(get_db_session),
 ):
     access_token = await UserService.login_user(
@@ -48,6 +50,18 @@ async def change_passsword():
     pass
 
 
-@router.patch('/logout')
-async def logout_user():
-    pass
+@router.delete('/logout', status_code=status.HTTP_204_NO_CONTENT)
+async def logout_user(
+    response: Response,
+    request: Request,
+    user: User = Depends(get_current_user)
+):
+    try:
+        response.delete_cookie(
+                key="access_token",
+                path="/"
+            )
+
+    except Exception as e:
+        # logger.error(f"Logout error for user {user.id}: {e}")
+        response.delete_cookie("access_token", path="/")
